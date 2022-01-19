0d059643cbb8f6e3497eec94c9c11d23b511f4dc
Gustavo <lgusta333@gmail.com>
2022-01-19 19:21:14 -0300

    Initial commit

---
+++ Biblioteca/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Usuario
+    {
+        public string name { get; set; }
+        public string cpf { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ DeserializarXML/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DeserializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Users\Gustavo\Documents\Arquivos c#\01_SerializarXML");
+
+            XmlSerializer xmlSrl = new XmlSerializer(typeof(Usuario));
+            Usuario user = (Usuario) xmlSrl.Deserialize(stream);
+
+            Console.WriteLine("User Name: {0}, CPF: {1}, Email: {2}", user.name, user.cpf, user.email);
+            Console.ReadKey();
+        }
+    }
+}
